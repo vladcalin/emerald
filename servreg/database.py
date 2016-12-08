aa25f4fbf4d4d397bceaa58a6415b5c18195d031
Calin Vlad <vlad.s.calin@gmail.com>
2016-12-09 00:29:28 +0200

    added service registration and retrieval

--- servreg/database.py
+++ servreg/database.py
@@ -1,11 +1,13 @@
 import datetime
+import humanize
 
-from sqlalchemy import create_engine, Column, String, Integer, Boolean, DateTime, Sequence
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, DateTime, Sequence, Index
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 Session = sessionmaker()
+
 
 def init_database(dburl):
     engine = create_engine(dburl)
@@ -23,5 +25,15 @@ class Service(Base):
     port = Column(Integer)
 
     first_seen = Column(DateTime, default=datetime.datetime.now)
-    last_seen = Column(DateTime)
+    last_seen = Column(DateTime, default=datetime.datetime.now)
 
+    __table_args__ = (Index("host_port_index", "host", "port", unique=True),)
+
+    def is_alive(self):
+        return (datetime.datetime.now() - self.last_seen).total_seconds() <= 60 * 3
+
+    def human_readable_first_seen(self):
+        return humanize.naturalday(self.first_seen)
+
+    def human_readable_last_seen(self):
+        return humanize.naturaltime(self.last_seen)
--- servreg/service.py
+++ servreg/service.py
@@ -12,12 +12,18 @@ import servreg.static
 import servreg.templates
 from servreg.database import init_database, Service, Base, Session
 
+STATIC_DIR = os.path.dirname(os.path.abspath(servreg.static.__file__))
+TEMPLATES_DIR = os.path.dirname(os.path.abspath(servreg.templates.__file__))
+print(STATIC_DIR)
+print(TEMPLATES_DIR)
+
 
 # example custom request handler
 class IndexHandler(RequestHandler):
     @coroutine
     def get(self):
-        self.render("index.html", version="0.0.1dev")
+        session = Session()
+        self.render("index.html", version="0.0.1dev", services=session.query(Service).filter())
 
 
 class ServiceRegistry(PyMicroService):
@@ -33,11 +39,11 @@ class ServiceRegistry(PyMicroService):
     ]
 
     # create your templates
-    template_dir = os.path.dirname(os.path.abspath(servreg.templates.__file__))
+    template_dir = TEMPLATES_DIR
 
     # setup your static files
     static_dirs = [
-        (r"/static", os.path.dirname(os.path.abspath(servreg.static.__file__))),
+        (r"/static", STATIC_DIR),
     ]
 
     def __init__(self, host, port, dburl):
@@ -51,8 +57,9 @@ class ServiceRegistry(PyMicroService):
         super(ServiceRegistry, self).__init__()
 
     @public_method
-    def register_service(self, name, host, port):
-
+    def ping(self, name, host, port):
+        """Acknowledges the existence of a service. If the service was previously registered (exists in db),
+        its stats will be updated (last ping time and name if does not match)"""
         session = Session()
         existent_service = session.query(Service).filter(Service.host == host, Service.port == port).all()
         if not existent_service:
@@ -60,22 +67,27 @@ class ServiceRegistry(PyMicroService):
         else:
             service = existent_service[0]
             service.last_seen = datetime.datetime.now()
+            service.name = name
 
         session.add(service)
         session.commit()
 
         return True
 
-    @private_api_method
-    def say_private_hello(self, name):
-        """
-        Retrieves a value that was previously stored.
-        """
-        return "Private hello {0}".format(name)
+    @public_method
+    def locate_service(self, name):
+        """Locates all existing services by a given glob expression"""
+        name = self.glob_to_sql(name)
+        session = Session()
+        return [{"host": service.host, "port": service.port}
+                for service in session.query(Service).filter(Service.name.like(name)) if service.is_alive()]
 
-    # Implement your token validation logic
-    def api_token_is_valid(self, api_token):
-        return True
+    def glob_to_sql(self, name):
+        if "*" in name:
+            name = name.replace("*", "%")
+        if "?" in name:
+            name = name.replace("?", "_")
+        return name
 
 
 @click.command()
